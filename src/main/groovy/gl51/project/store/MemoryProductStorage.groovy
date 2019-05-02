7bc15911fb2cbe261198b5c1c7698e0b33ae77f0
Czq96 <ohce69cc@gmail.com>
2019-05-02 21:41:45 +0200

    store product CRUD test

--- src/main/groovy/gl51/project/store/MemoryProductStorage.groovy
+++ src/main/groovy/gl51/project/store/MemoryProductStorage.groovy
@@ -1,28 +1,55 @@
 package gl51.project.store
 
 class MemoryProductStorage implements  ProductStorage {
+
+    List<Product> products = []
+
     @Override
     void save(Product p) {
-
+        this.products.add(p)
     }
 
     @Override
     void update(String id, Product p) {
-
+        try{
+            for(int i=0;i<products.size();i++)
+            {
+                if(products[i].id==id)
+                {
+                    products[i] = p
+                }
+            }
+        }catch(Exception ex){
+            println("no product with id "+id)
+        }
     }
 
     @Override
     Product getByID(String id) {
-        return null
+        try{
+            for(int i=0;i<products.size();i++)
+            {
+                if(products[i].id==id)
+                {
+                    return products[i]
+                }
+            }
+        }catch(Exception ex){
+            println("no product with id "+id)
+        }
     }
 
     @Override
     void delete(String id) {
-
+        try{
+            products.remove(this.getByID(id))
+        }catch(Exception ex){
+            println("no product with id "+id)
+        }
     }
 
     @Override
     List<Product> all() {
-        []
+        return products
     }
 }
--- src/test/groovy/gl51/project/store/MemoryProductStorageTest.groovy
+++ src/test/groovy/gl51/project/store/MemoryProductStorageTest.groovy
@@ -7,8 +7,8 @@ class MemoryProductStorageTest extends Specification {
     ProductStorage store = new MemoryProductStorage()
 
     def "empty storage returns empty list"() {
-        expect:
-        store.all() ==  []
+         expect:
+         store.all() ==  []
     }
 
     def "adding a product returns the product in the list"() {
@@ -22,4 +22,36 @@ class MemoryProductStorageTest extends Specification {
         all.size() == 1
         all.first().name == 'myproduct'
     }
+
+    def "update a product in the list returns a new list"() {
+        store.save(new Product(id: "1", name: "myproduct"))
+        Product newProduct = new Product(
+                id: "10",
+                name: "myproduct updated",
+                description: "new product",
+                price: 2.50,
+                idealTemperature: 10)
+        setup:
+        store.update("1", newProduct)
+
+        when:
+        def proInStore = store.getByID(newProduct.id)
+
+        then:
+        proInStore == newProduct
+    }
+
+    def "get a product by id"() {
+        store.save(new Product(id: "1", name: "myproduct"))
+        expect:
+        store.getByID("1").name=='myproduct'
+    }
+
+    def "delete a product by id"() {
+        store.save(new Product(id: "1", name: "myproduct"))
+        store.delete("1")
+        expect:
+        store.all() == []
+    }
+
 }
